9f75b1cb00d5bfd15d3bcba0d358d42f895e68be
lathamadakkannu <lathamadakkannu@gmail.com>
2023-07-27 16:30:33 -0700

    addnew

---
+++ main.js
@@ -0,0 +1,5 @@
+var a=prompt("Enter the word");
+document.write("The Given Word is "+a+"<br>");
+var c=a.replace(a[0],a[0].toUpperCase());
+document.write("The Capitalized Word Is ")
+document.write(c);
